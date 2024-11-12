57db32e7424e609d36cfb59a4a329c75129b0404
mkaufman385 <mkaufman385@gmail.com>
2024-11-12 18:33:55 -0500

    localhost working properly now. Adjusted setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,24 +1,10 @@
-// const { createProxyMiddleware } = require("http-proxy-middleware");
-
-// module.exports = function (app) {
-//   app.use(
-//     "/connect/token",
-//     createProxyMiddleware({
-//       target: "https://oauth.fatsecret.com",
-//       changeOrigin: true,
-//     })
-//   );
-// };
-
-// ------------------------------------------------------------------------------
-
-const proxy = require("http-proxy-middleware");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   // Proxy for token request
   app.use(
     "/connect",
-    proxy({
+    createProxyMiddleware({
       target: "https://oauth.fatsecret.com",
       changeOrigin: true,
       secure: false,
@@ -28,10 +14,36 @@ module.exports = function (app) {
   // Proxy for food data request
   app.use(
     "/rest",
-    proxy({
+    createProxyMiddleware({
       target: "https://platform.fatsecret.com",
       changeOrigin: true,
       secure: false,
     })
   );
 };
+
+// ------------------------------------------------------------------------------
+
+// const proxy = require("http-proxy-middleware");
+
+// module.exports = function (app) {
+//   // Proxy for token request
+//   app.use(
+//     "/connect",
+//     proxy({
+//       target: "https://oauth.fatsecret.com",
+//       changeOrigin: true,
+//       secure: false,
+//     })
+//   );
+
+//   // Proxy for food data request
+//   app.use(
+//     "/rest",
+//     proxy({
+//       target: "https://platform.fatsecret.com",
+//       changeOrigin: true,
+//       secure: false,
+//     })
+//   );
+// };
