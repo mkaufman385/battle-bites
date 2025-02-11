837c2e1924cb2a3bd342e8394e1da973981ff05c
mkaufman385 <mkaufman385@gmail.com>
2025-02-11 18:44:17 -0500

    wip. package.json and setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,3 +1,30 @@
+// const { createProxyMiddleware } = require("http-proxy-middleware");
+
+// module.exports = function (app) {
+//   // Proxy for token request
+//   app.use(
+//     "/connect",
+//     createProxyMiddleware({
+//       target: "https://oauth.fatsecret.com",
+//       changeOrigin: true,
+//       secure: false,
+//       logLevel: "debug",
+//     })
+//   );
+
+//   // Proxy for food data request
+//   app.use(
+//     "/rest",
+//     createProxyMiddleware({
+//       target: "https://platform.fatsecret.com",
+//       changeOrigin: true,
+//       secure: false,
+//       logLevel: "debug",
+//     })
+//   );
+// };
+
+// ------------------------------------------------------------------------------
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
@@ -7,8 +34,8 @@ module.exports = function (app) {
     createProxyMiddleware({
       target: "https://oauth.fatsecret.com",
       changeOrigin: true,
-      secure: false,
-      logLevel: "debug",
+      secure: false, // or set to true if the target has a valid SSL cert
+      logLevel: "info", // Change to 'info' for less detailed logging
     })
   );
 
@@ -18,10 +45,8 @@ module.exports = function (app) {
     createProxyMiddleware({
       target: "https://platform.fatsecret.com",
       changeOrigin: true,
-      secure: false,
-      logLevel: "debug",
+      secure: false, // or set to true if the target has a valid SSL cert
+      logLevel: "info", // Change to 'info' for less detailed logging
     })
   );
 };
-
-// ------------------------------------------------------------------------------
